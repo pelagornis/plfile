4ca808ebdcb95df0c363c83257b4799afa79c3c3
안지훈 <jihoonahn.dev@gmail.com>
2024-04-06 18:23:44 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,14 +3,19 @@ import PackageDescription
 
 let package = Package(
     name: "plfile",
-    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+    platforms: [
+        .iOS(.v13), 
+        .macOS(.v10_15), 
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(
             name: "File",
             targets: ["File"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.3.0")
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
     ],
     targets: [
         .target(name: "File"),
